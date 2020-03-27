3f695b47470805c4d51c6f0ad4e5e7b371070414
Mack McQuen <mackmcquen@comcast.net>
2020-03-27 20:49:31 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -3,8 +3,8 @@ def map_to_negativize(array)
   array = []
   count = 0
   while count < array.size do
-   array.push(array[count] * -1)
-  count += 1
-en  d
+    array.push(array[count] * -1)
+    count += 1
+end
 array
 end
